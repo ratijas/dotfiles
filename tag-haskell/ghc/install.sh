e315ccc8a383eb5e215b990cdb23b5c9fa36f28b
ivan tkachenko <ratijas@users.noreply.github.com>
2018-09-30 04:11:07 +0300

    Haskell GHCi GOA

--- tag-haskell/ghc/install.sh
+++ tag-haskell/ghc/install.sh
@@ -1,3 +1,10 @@
-stack --resolver=lts-12.10 install ipprint-0.6 sr-extra-1.46.3.2 Unixutils-1.54.1
+local resolver=lts-12.10
+
+stack --resolver="${resolver}" setup
+
+stack --resolver="${resolver}" install ipprint-0.6 sr-extra-1.46.3.2 Unixutils-1.54.1
 stack install hoogle
 hoogle generate
+
+PATH="$PATH:$(stack --resolver="${resolver}" path --bin-path)" \
+	cabal install lambdabot goa
