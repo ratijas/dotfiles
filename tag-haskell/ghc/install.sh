ab28875a68d8b406accc49ec64706044e67f28cf
ivan tkachenko <ratijas@users.noreply.github.com>
2018-11-13 23:33:07 +0300

    haskell: bump stackage to 12.16

--- tag-haskell/ghc/install.sh
+++ tag-haskell/ghc/install.sh
@@ -1,10 +1,16 @@
-local resolver=lts-12.10
+#!/bin/sh
+
+resolver=lts-12.16
 
 stack --resolver="${resolver}" setup
 
-stack --resolver="${resolver}" install ipprint-0.6 sr-extra-1.46.3.2 Unixutils-1.54.1
-stack install hoogle
+stack --resolver="${resolver}" install \
+	ipprint-0.6 \
+	sr-extra-1.46.3.2 \
+	Unixutils-1.54.1 \
+	goa-3.3 \
+	hoogle
 hoogle generate
 
 PATH="$PATH:$(stack --resolver="${resolver}" path --bin-path)" \
-	cabal install lambdabot goa
+	cabal install lambdabot
