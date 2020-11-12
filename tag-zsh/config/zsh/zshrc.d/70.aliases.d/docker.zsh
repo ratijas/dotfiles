080458300cb33e17019d6e62b1e7f66a8555d18b
ivan tkachenko <me@ratijas.tk>
2020-11-12 22:02:53 +0300

    zsh: Copy docker-compose aliases from oh-my-zsh
    Turns out, docker and docker-compose completion plugins are exactly
    the same as the ones shipped with the docker package, so I don't need
    them here.

--- tag-zsh/config/zsh/zshrc.d/70.aliases.d/docker.zsh
+++ tag-zsh/config/zsh/zshrc.d/70.aliases.d/docker.zsh
@@ -1,5 +1,28 @@
+# Docker aliases
+
 alias dps="docker ps"
 alias dpsa="docker ps -a"
 
-# monitoring docker
+# Monitoring
 alias sen="docker run -v /var/run/docker.sock:/run/docker.sock -it --rm -e TERM tomastomecek/sen"
+
+
+# Docker-compose aliases
+
+alias dco='docker-compose'
+
+alias dcb='docker-compose build'
+alias dce='docker-compose exec'
+alias dcps='docker-compose ps'
+alias dcrestart='docker-compose restart'
+alias dcrm='docker-compose rm'
+alias dcr='docker-compose run'
+alias dcstop='docker-compose stop'
+alias dcup='docker-compose up'
+alias dcupd='docker-compose up -d'
+alias dcdn='docker-compose down'
+alias dcl='docker-compose logs'
+alias dclf='docker-compose logs -f'
+alias dcpull='docker-compose pull'
+alias dcstart='docker-compose start'
+alias dck='docker-compose kill'
