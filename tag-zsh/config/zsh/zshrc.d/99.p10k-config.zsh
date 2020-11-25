0198db2ce667f02f1983f4d8c7ad22d3ca4b32b2
ivan tkachenko <me@ratijas.tk>
2020-11-25 05:41:12 +0300

    zsh: for now, let's have two possible locations for p10k

--- tag-zsh/config/zsh/zshrc.d/99.p10k-config.zsh
+++ tag-zsh/config/zsh/zshrc.d/99.p10k-config.zsh
@@ -1,6 +1,9 @@
 [[ -f    /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme ]] &&
   source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 
+[[ -f    ~/powerlevel10k/powerlevel10k.zsh-theme ]] &&
+  source ~/powerlevel10k/powerlevel10k.zsh-theme
+
 # To customize prompt, run `p10k configure` or edit p10k.zsh.
 [[ -f    ~/.config/zsh/p10k.zsh ]] &&
   source ~/.config/zsh/p10k.zsh
