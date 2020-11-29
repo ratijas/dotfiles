270ac0f19347b31e480ca6c98f49b869d29a2a83
ivan tkachenko <me@ratijas.tk>
2020-11-29 18:27:07 +0300

    zsh: add alias for git archive

--- tag-zsh/config/zsh/zshrc.d/70.aliases.d/git.zsh
+++ tag-zsh/config/zsh/zshrc.d/70.aliases.d/git.zsh
@@ -1,2 +1,4 @@
 # probably did not intend to run GS
 alias gs="echo 'Did you mean $(which gst)?'"
+
+alias git-archive-zip='git archive --prefix="${PWD:t}" --format=zip --output "${PWD:t}.zip" HEAD'
