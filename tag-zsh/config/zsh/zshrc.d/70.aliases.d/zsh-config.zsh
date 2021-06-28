16edddfe1b5eadfd9272bf35b1d953b62842e0de
ivan tkachenko <me@ratijas.tk>
2021-06-28 17:34:50 +0300

    zsh: fix config aliases

--- tag-zsh/config/zsh/zshrc.d/70.aliases.d/zsh-config.zsh
+++ tag-zsh/config/zsh/zshrc.d/70.aliases.d/zsh-config.zsh
@@ -1,5 +1,5 @@
 # General zsh utils
-alias zshconfig="$VISUAL $HOME/.zshrc"
-alias zshconfigd="$VISUAL $HOME/.zshrc.d/"
-alias zshaliases="$VISUAL $HOME/.zshrc.d/70.aliases"
-alias zshaliasesd="$VISUAL $HOME/.zshrc.d/70.aliases.d/"
+alias zshconfig="  \$VISUAL $HOME/.zshrc"
+alias zshconfigd=" \$VISUAL $HOME/.config/zsh/zshrc.d/"
+alias zshaliases=" \$VISUAL $HOME/.config/zsh/zshrc.d/70.aliases"
+alias zshaliasesd="\$VISUAL $HOME/.config/zsh/zshrc.d/70.aliases.d/"
