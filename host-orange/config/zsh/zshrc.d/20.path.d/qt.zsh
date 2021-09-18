97fe432c902ce709794fa224536c72c65cac1fad
ivan tkachenko <me@ratijas.tk>
2021-09-18 17:19:14 +0300

    zsh: update Qt location; don't pollute PATH unnecessarily

--- host-orange/config/zsh/zshrc.d/20.path.d/qt.zsh
+++ host-orange/config/zsh/zshrc.d/20.path.d/qt.zsh
@@ -1,2 +1,7 @@
-export PATH=$PATH:$HOME/projects/qt/qt5/qtrepotools/bin
-export PATH=$PATH:/opt/Qt/Tools/Ninja
+if [[ -d $HOME/projects/q ]]; then
+    export PATH=$PATH:$HOME/projects/q/qt5/qtrepotools/bin
+fi
+
+if [[ -d /opt/Qt/Tools/Ninja ]]; then
+    export PATH=$PATH:/opt/Qt/Tools/Ninja
+fi
--- tag-zsh/config/zsh/zshrc.d/20.path.d/android.zsh
+++ tag-zsh/config/zsh/zshrc.d/20.path.d/android.zsh
@@ -1,5 +1,7 @@
-export ANDROID_SDK_ROOT=/opt/android-sdk
-export ANDROID_HOME=$ANDROID_SDK_ROOT
-export ANDROID_NDK=$ANDROID_SDK_ROOT/ndk-bundle
-export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
-export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
+if [[ -d /opt/android-sdk ]]; then
+    export ANDROID_SDK_ROOT=/opt/android-sdk
+    export ANDROID_HOME=$ANDROID_SDK_ROOT
+    export ANDROID_NDK=$ANDROID_SDK_ROOT/ndk-bundle
+    export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+    export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
+fi
