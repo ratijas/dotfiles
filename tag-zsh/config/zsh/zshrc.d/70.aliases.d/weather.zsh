72a3235c04055b622db40086810e2e9a2314cdad
ivan tkachenko <me@ratijas.tk>
2021-09-22 21:57:50 +0300

    zsh: improve weather script to handle multi-word locations

--- tag-zsh/config/zsh/zshrc.d/70.aliases.d/weather.zsh
+++ tag-zsh/config/zsh/zshrc.d/70.aliases.d/weather.zsh
@@ -1,3 +1,12 @@
 # Local weather
-function weather() { curl http://wttr.in/"$1" }
+function weather() {
+  local location=$(echo -n $@ | python -c '
+import sys
+from urllib.request import pathname2url
+
+sys.stdout.write(pathname2url(sys.stdin.read()))
+')
+  curl http://wttr.in/"$location"
+}
+
 alias inno="weather Иннополис"
