c2b4aefe5ff36b7d891067a743b08fee86cf2df1
ivan tkachenko <me@ratijas.tk>
2022-11-14 02:45:36 +0300

    zsh: Add some zstyle ':completion:*:descriptions' formats from the internet

--- tag-zsh/config/zsh/zshrc.d/05.complete.zsh
+++ tag-zsh/config/zsh/zshrc.d/05.complete.zsh
@@ -4,6 +4,20 @@
 zstyle ':completion:*' menu select
 # Make it case insensetive
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Color completion for some things.
+# http://linuxshellaccount.blogspot.com/2008/12/color-completion-using-zsh-modules-on.html
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# formatting and messages
+# http://www.masterzen.fr/2009/04/19/in-love-with-zsh-part-one/
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format "$fg[yellow]%B--- %d%b"
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format "$fg[red]No matches for:$reset_color %d"
+zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+zstyle ':completion:*' group-name ''
+
 # Speed up compinit
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/cache"
