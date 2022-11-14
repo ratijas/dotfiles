0419543ac3f4699b483cd590b434cace6575c50d
ivan tkachenko <me@ratijas.tk>
2022-11-14 14:29:03 +0300

    zsh: sync lol plugin to OMZ upstream
    https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/lol

--- tag-zsh/config/zsh/zshrc.d/50.plugins.d/lol/lol.plugin.zsh
+++ tag-zsh/config/zsh/zshrc.d/50.plugins.d/lol/lol.plugin.zsh
@@ -1,7 +1,7 @@
 # LOL!!1
 # Source: https://aur.archlinux.org/packages/lolbash/lolbash/lolbash.sh
 
-alias wtf='dmesg -w'
+alias wtf='dmesg'
 alias onoz='cat /var/log/errors.log'
 alias rtfm='man'
 
@@ -45,7 +45,6 @@ alias bringz='git pull'
 alias chicken='git add'
 alias oanward='git commit -m'
 alias ooanward='git commit -am'
-alias yolo='git commit -m "$(curl -s http://whatthecommit.com/index.txt)"'
+alias yolo='git commit -m "$(curl -s https://whatthecommit.com/index.txt)"'
 alias letcat='git checkout'
 alias violenz='git rebase'
-
