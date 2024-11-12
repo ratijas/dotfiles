be308a12e7c59631a3c666d3bd4cb33f0005aaa9
ivan tkachenko <me@ratijas.tk>
2024-11-13 03:34:59 +0500

    add Android and emsdk

--- tag-zsh/config/zsh/zshrc.d/20.path.d/android.zsh
+++ tag-zsh/config/zsh/zshrc.d/20.path.d/android.zsh
@@ -5,3 +5,12 @@ if [[ -d /opt/android-sdk ]]; then
     export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
     export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
 fi
+
+if [[ -d /home/ratijas/Android/Sdk ]]; then
+    export ANDROID_SDK_ROOT=/home/ratijas/Android/Sdk
+    export ANDROID_NDK=$ANDROID_SDK_ROOT/ndk/$(ls -1 $ANDROID_SDK_ROOT/ndk | head -1)
+    export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+    export PATH=$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin
+    export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
+    export PATH=$PATH:$ANDROID_SDK_ROOT/build-tools/35.0.0/
+fi
---
+++ tag-zsh/config/zsh/zshrc.d/20.path.d/emsdk.zsh
@@ -0,0 +1,4 @@
+if [[ -d /usr/lib/emsdk ]]; then
+    export PATH=$PATH:/usr/lib/emsdk
+    export PATH=$PATH:/usr/lib/emsdk/upstream/emscripten
+fi
